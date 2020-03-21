82b04e45ddbc5377854201f544caf44391787116
mariaclaraseixa <62485686+mariaclaraseixa@users.noreply.github.com>
2020-03-21 18:33:17 -0300

    My first upload
    Using social distance promoted at Joinville - SC - Brazil to learn a little bit about programming

---
+++ estadoFisico.c
@@ -0,0 +1,100 @@
+/**
+*  A tabela a seguir contém temperatura de fusão e de ebulição
+* de várias substâncias. Complete a tabela com o estado físico
+* de cada uma a 25 graus Celsius.
+*
+* Exemplo de dados fornecidos pelo usuário:
+* 
+* Substância   tf     te   Estado físico
+* ---------- ----   ----  --------------
+* A           -83     -4   ?
+* B           -47    +16   ?
+* C           -21    +44   ?
+* D           +18    +65   ?
+* E           +83   +397   ?
+*
+* Onde:
+* - tf -> temperatura de fusão em graus Celsius
+* - te -> temperatura de ebulição em graus Celsius
+* - ?  -> Sólido/Líquido/Gasoso
+*
+* Observações sobre o programa:
+*  - char[ NUMERO ] -> Sequencia de caracteres (uma String)
+*                      serve para guardar palavras e frases
+*                      na memória. Reserva um NUMERO de
+*                      espaços na memória para caracteres
+*  - float          -> indica que queremos guardar um número
+*                      do domínio dos reais, ou seja, com 
+*                      casas decimais após a vírgula podendo
+*                      ser positivo ou negativo
+*
+*  - int             -> representa um valor inteiro, positivo
+*                       ou negativo.
+*
+*  ---------------------------------------------------------
+*
+*  - scanf          -> solicita para o usuário digitar um valor
+*                      via teclado
+*  
+*  CUIDADO: A variável do scanf quando ela possui [] não deve ter
+*           o caracter & (olhe abaixo no scanf nome e o 
+*           scanf &temperaturaDeFusao
+*           NAO COLOQUE ACENTO EM NOME DE VARIÁVEL... ISSO É O 1o 
+*           AVISO... NO SEGUNDO HAVERÁ CONSEQUÊNCIAS...
+*/
+
+#define SOLIDO  0
+#define LIQUIDO 1
+#define GASOSO  2
+
+#define TEMPERATURA_ATUAL 25
+
+#include <stdio.h>
+
+int qualEstadoDaMateria( int tf, int te, int temperaturaCorrente ) {
+    // se a temperatura corrente for menor que a temepratura de fusão
+    if( temperaturaCorrente < tf ) {
+        return SOLIDO;
+    }
+    // se a temperaturacorrente for maior que a temperatura de ebulição
+    if( temperaturaCorrente > te ) {
+       return GASOSO;
+    }
+    return LIQUIDO;
+} 
+
+int main(int argc, char *argv[]) {
+    char nome[20]; // array de char é uma String por isso %s no scanf
+
+    float temperaturaFusao; // %f no scanf
+    float temperaturaEbulicao;
+
+    int estado; // %d no scanf
+
+    printf("\nQual o nome da substância? ");
+    scanf("%s", nome);
+
+    printf("\nQual a temperatura de fusão (tf)? ");
+    scanf("%f", &temperaturaFusao);   
+    
+    printf("\nQual a temperatura de ebulição (te)? " );
+    scanf("%f", &temperaturaEbulicao);
+     
+    estado = qualEstadoDaMateria( temperaturaFusao, temperaturaEbulicao, TEMPERATURA_ATUAL); 
+    printf("O valor da variavel estado é %d \n", estado );
+
+    switch( estado ) {
+        case SOLIDO:
+          printf("\nSolido\n");
+          break;
+        case LIQUIDO:
+          printf("\nLiquido\n");
+          break;
+        case GASOSO:
+          printf("\nGasoso\n");
+          break;
+        default:
+          printf("Xiiii algo deu errado mano!!! Vai saber˜!!!");
+          break;
+    }
+}
---
+++ helloMaria.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+
+/**
+* Escreve meu nome usando ASCII art
+* Demonstra como usar função na linguagem C
+*
+* Uma função é um conjunto de comandos que realiza uma tarefa específica
+*
+*
+* Baseado no texto de http://patorjk.com/software/taag/#p=display&f=Doh&t=Maria
+*
+* Tarefa: ler https://homepages.dcc.ufmg.br/~rodolfo/aedsi-2-10/Funcoes/funcoes.html
+*
+* @author: Maria Clara Seixa Scheffel
+* @email: mariaclara.seixa at gmail
+*/
+
+
+/**
+* Função para escrever a palavra Maria
+*/
+void escreverMaria() {
+  printf("MMMMMMMM               MMMMMMMM                                      iiii                    \n");
+  printf("M:::::::M             M:::::::M                                     i::::i                   \n");
+  printf("M::::::::M           M::::::::M                                      iiii                    \n");
+  printf("M:::::::::M         M:::::::::M                                                              \n");
+  printf("M::::::::::M       M::::::::::M  aaaaaaaaaaaaa  rrrrr   rrrrrrrrr  iiiiiii   aaaaaaaaaaaaa   \n");
+  printf("M:::::::::::M     M:::::::::::M  a::::::::::::a r::::rrr:::::::::r i:::::i   a::::::::::::a  \n");
+  printf("M:::::::M::::M   M::::M:::::::M  aaaaaaaaa:::::ar:::::::::::::::::r i::::i   aaaaaaaaa:::::a \n");
+  printf("M::::::M M::::M M::::M M::::::M           a::::arr::::::rrrrr::::::ri::::i            a::::a \n");
+  printf("M::::::M  M::::M::::M  M::::::M    aaaaaaa:::::a r:::::r     r:::::ri::::i     aaaaaaa:::::a \n");
+  printf("M::::::M   M:::::::M   M::::::M  aa::::::::::::a r:::::r     rrrrrrri::::i   aa::::::::::::a \n");
+  printf("M::::::M    M:::::M    M::::::M a::::aaaa::::::a r:::::r            i::::i  a::::aaaa::::::a \n");
+  printf("M::::::M     MMMMM     M::::::Ma::::a    a:::::a r:::::r            i::::i a::::a    a:::::a \n");
+  printf("M::::::M               M::::::Ma::::a    a:::::a r:::::r           i::::::ia::::a    a:::::a \n");
+  printf("M::::::M               M::::::Ma:::::aaaa::::::a r:::::r           i::::::ia:::::aaaa::::::a \n");
+  printf("M::::::M               M::::::M a::::::::::aa:::ar:::::r           i::::::i a::::::::::aa:::a\n");
+  printf("MMMMMMMM               MMMMMMMM  aaaaaaaaaa  aaaarrrrrrr           iiiiiiii  aaaaaaaaaa  aaaa\n");
+}
+
+/**
+* Função para escrever a palavra Clara
+*/
+void escreverClara() {
+  printf("         CCCCCCCCCCCCClllllll                                                       \n");
+  printf("      CCC::::::::::::Cl:::::l                                                       \n");
+  printf("    CC:::::::::::::::Cl:::::l                                                       \n");
+  printf("   C:::::CCCCCCCC::::Cl:::::l                                                       \n");
+  printf("  C:::::C       CCCCCC l::::l   aaaaaaaaaaaaa  rrrrr   rrrrrrrrr   aaaaaaaaaaaaa    \n");
+  printf(" C:::::C               l::::l   a::::::::::::a r::::rrr:::::::::r  a::::::::::::a   \n");
+  printf(" C:::::C               l::::l   aaaaaaaaa:::::ar:::::::::::::::::r aaaaaaaaa:::::a  \n");
+  printf(" C:::::C               l::::l            a::::arr::::::rrrrr::::::r         a::::a  \n");
+  printf(" C:::::C               l::::l     aaaaaaa:::::a r:::::r     r:::::r  aaaaaaa:::::a  \n");
+  printf(" C:::::C               l::::l   aa::::::::::::a r:::::r     rrrrrrraa::::::::::::a  \n");
+  printf(" C:::::C               l::::l  a::::aaaa::::::a r:::::r           a::::aaaa::::::a  \n");
+  printf("  C:::::C       CCCCCC l::::l a::::a    a:::::a r:::::r          a::::a    a:::::a  \n");
+  printf("   C:::::CCCCCCCC::::Cl::::::la::::a    a:::::a r:::::r          a::::a    a:::::a  \n");
+  printf("    CC:::::::::::::::Cl::::::la:::::aaaa::::::a r:::::r          a:::::aaaa::::::a  \n");
+  printf("      CCC::::::::::::Cl::::::l a::::::::::aa:::ar:::::r           a::::::::::aa:::a \n");
+  printf("         CCCCCCCCCCCCCllllllll  aaaaaaaaaa  aaaarrrrrrr            aaaaaaaaaa  aaaa \n");
+}
+
+/**
+* Função para escrever meu nome completo
+*/
+void escreverMeuNome() {
+  escreverMaria();
+  escreverClara();
+}
+
+
+
+// o programa começa executando no main
+int main() {
+  printf("Saindo do main -> \n\n\n"); // Algumas linhas em branco no início
+  
+  escreverMeuNome();  
+
+
+  printf("Voltou para o  main -> \n\n\n"); // Algumas linhas em branco no início
+}
